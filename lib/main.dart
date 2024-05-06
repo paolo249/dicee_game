8840ca301c3d2a481e79c601457bfd3e5cc84430
Paolo <paologarciaang@gmail.com>
2024-05-05 18:16:31 -0700

    Add functionality to make dice image change reactively

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,25 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+
+
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 5;
+  
 
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
-        
         children: <Widget>[
           Expanded(
             child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/dice1.png'),
+            child: TextButton(onPressed: () {
+              setState(() {
+                leftDiceNumber = Random().nextInt(6)+1;
+              
+              });
+               
+
+             },
+            child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           ),
           Expanded(
             child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Image.asset('Images/dice2.png')
+            child: TextButton(onPressed: () { 
+              setState(() {
+                rightDiceNumber = Random().nextInt(6)+1;
+                
+              });
+             },
+            child: Image.asset('Images/dice$rightDiceNumber.png')),
           ),
           ),
         ],
